d3e4fab13f76b42eab5cf6e4ed5274598621344a
Chris Boylen <billpolster@gmail.com>
2018-08-19 19:00:58 -0600

    Add and pass test so that state is updated when selectcard is invoked on app

--- src/Components/App.test.js
+++ src/Components/App.test.js
@@ -69,4 +69,30 @@ describe('APP', () => {
     expect(wrapper.state().data.length).toEqual(2);
   })
 
+  it('Should update state when selectCard is invoked', () => {
+    const mockData = 'COLORADO'
+    const expected = [{
+      "location": "COLORADO",
+      "isSelected": true,
+      "stats": {
+        "2004": 0.24,
+        "2005": 0.278,
+        "2006": 0.337,
+        "2007": 0.395,
+        "2008": 0.536,
+        "2009": 0.598,
+        "2010": 0.64,
+        "2011": 0.672,
+        "2012": 0.695,
+        "2013": 0.703,
+        "2014": 0.741
+      }
+    }];
+
+    wrapper.instance().selectCard(mockData)
+
+    expect(wrapper.state().selectedCards).toEqual(expected);
+    expect(wrapper.state().selectedCards.length).toEqual(1);
+  })
+
 })
--- src/Components/SelectedCont.test.js
+++ src/Components/SelectedCont.test.js
@@ -1,18 +1,45 @@
 import React from 'react';
 import { shallow, mount } from 'enzyme';
 import SelectedCont from './SelectedCont';
-import Card from './Card';
 
 describe('SELECTEDCONT', () => {
-  it('Should match snapshot', () => {
+  it('Should match snapshot when there is an empty array', () => {
     const wrapper = shallow (
-      <Card 
-        location='colorado'
-        stats={{key: 1}}
-        key={1}
+      <SelectedCont
+        selectedCards={[]}
       />
     )
 
     expect(wrapper.html()).toMatchSnapshot();
-  }) 
+  })
+  
+  it('Should match snapshot when there is data in the array', () => {
+    const mockData = [
+      {
+        location: 'COLORADO',
+        isSelected: true,
+        stats: {
+          2002: 1
+        }, 
+      },
+      {
+        location: 'BINGO',
+        isSelected: true,
+        stats: {
+          2002: 1
+        }
+      }
+    ];
+    
+    const compareDistrictAveragesMock = jest.fn()
+
+    const wrapper = shallow(
+      <SelectedCont 
+        selectedCards={mockData} 
+        compareDistrictAverages={compareDistrictAveragesMock}
+      />
+    );
+
+    expect(wrapper.html()).toMatchSnapshot();
+  })
 })
