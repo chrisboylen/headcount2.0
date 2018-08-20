ca7ecfc70fd7d16708204c8e07b340f7d8e53e3f
Chris Boylen <billpolster@gmail.com>
2018-08-19 19:08:38 -0600

    Add and pass test so that state is updated when selectCard is invoked twice

--- src/Components/App.test.js
+++ src/Components/App.test.js
@@ -95,4 +95,49 @@ describe('APP', () => {
     expect(wrapper.state().selectedCards.length).toEqual(1);
   })
 
+  it('Should update state when selectCard is invoked twice', () => {
+    const mockData = 'COLORADO';
+    const mockData2 = 'ASPEN 1';
+    const expected = [{
+      "location": "COLORADO",
+      "isSelected": true,
+      "stats": {
+        "2004": 0.24,
+        "2005": 0.278,
+        "2006": 0.337,
+        "2007": 0.395,
+        "2008": 0.536,
+        "2009": 0.598,
+        "2010": 0.64,
+        "2011": 0.672,
+        "2012": 0.695,
+        "2013": 0.703,
+        "2014": 0.741
+      }
+    },
+    {
+     "location": "ASPEN 1",
+      "isSelected": true,
+        "stats": {
+          "2004": 1,
+          "2005": 1,
+          "2006": 1,
+          "2007": 1,
+          "2008": 1,
+          "2009": 1,
+          "2010": 1,
+          "2011": 1,
+          "2012": 1,
+          "2013": 1,
+          "2014": 0.992,
+        },
+    }];
+
+    wrapper.instance().selectCard(mockData)
+    wrapper.instance().selectCard(mockData2)
+
+    expect(wrapper.state().selectedCards).toEqual(expected);
+    expect(wrapper.state().selectedCards.length).toEqual(2);
+  })
+
 })
